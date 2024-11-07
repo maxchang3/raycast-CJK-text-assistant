21813fb13c6d104aae91792dcb3ddbd25e0873b9
MaxChang3 <maxcfoo@gmail.com>
2024-11-07 19:20:12 +0800

    refactor: rename and enhance text formatting commands and logic

--- src/improve-copied.ts
+++ src/improve-copied.ts
@@ -1,15 +1,25 @@
 import { Toast, showHUD, showToast, Clipboard, getSelectedText, closeMainWindow } from "@raycast/api"
-import pangu from "pangu"
+import { formatters } from "./lib"
 
 export default async function main() {
   try {
     const selection = await getSelectedText()
+    const newlineAtEnd = selection.at(-1) === "\n"
     if (selection.trim().length === 0) {
       showHUD("No valid text is selected.")
       closeMainWindow()
       return
     }
-    Clipboard.paste(pangu.spacing(selection))
+    let outputText = selection
+    Object.values(formatters).forEach((textHandler) => {
+      if (textHandler.activate) {
+        outputText = textHandler.executor(outputText)
+      }
+    })
+    if (newlineAtEnd) {
+      outputText += "\n"
+    }
+    Clipboard.paste(outputText)
     showHUD("Text has been auto-corrected.")
     closeMainWindow()
   } catch (error) {
